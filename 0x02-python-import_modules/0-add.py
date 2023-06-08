6cf1f9f9bf583aadd5850f1b073e620eba8d90b3
patrice02 <patricedev02@gmail.com>
2023-06-08 14:40:04 +0000

    add docstrings

--- 0x02-python-import_modules/0-add.py
+++ 0x02-python-import_modules/0-add.py
@@ -1,6 +1,8 @@
 #!/usr/bin/python3
-from add_0 import add
 if __name__ == "__main__":
+    from add_0 import add
+    """print the value of a + b"""
+
     a = 1
     b = 2
     print("{:d} + {:d} = {}".format(a, b, add(a, b)))
