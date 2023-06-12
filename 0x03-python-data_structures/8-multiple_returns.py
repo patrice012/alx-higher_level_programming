7e51bd26e02a36d2a08a794e6178dd611520d6cd
patrice02 <patricedev02@gmail.com>
2023-06-12 22:07:16 +0000

    update task 8

--- 0x03-python-data_structures/8-multiple_returns.py
+++ 0x03-python-data_structures/8-multiple_returns.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 
 def multiple_returns(sentence):
-    if sentence == "":
-        return (0, None)
-    return (len(sentence), sentence[0])
+    first_char = None
+    if sentence != "":
+        first_char = sentence[0]
+    return (len(sentence), first_char)
