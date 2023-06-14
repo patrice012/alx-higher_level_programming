ab11917211f6e9c07b47ff871f357b74046edb66
patrice02 <patricedev02@gmail.com>
2023-06-14 01:00:48 +0000

    update

--- 0x03-python-data_structures/13-is_palindrome.c
+++ 0x03-python-data_structures/13-is_palindrome.c
@@ -1,6 +1,8 @@
 #include "lists.h"
 #include <stdio.h>
 
+void free_arr(int **arr, int size);
+
 /**
   * is_palindrome - check if list is palindrome
   * @head: list head
@@ -9,35 +11,59 @@
 
 int is_palindrome(listint_t **head)
 {
-	int i = 0, j, k, size = 0;
-	listint_t *temp;
+	int i = 0, j, k, size = 0, **array;
+	listint_t *temp = *head;
 
-	temp = *head;
 	if (!(*head) || !(*head)->next)
 		return (1);
-
 	while (temp)
 	{
 		size++;
 		temp = temp->next;
 	}
-
-	int array[size];
-
+	array = (int **)malloc(sizeof(int *) * (size + 1));
+	if (!array)
+		return (0);
 	temp = *head;
 	while (temp)
 	{
-		array[i] = temp->n;
+		array[i] = (int *)malloc(sizeof(int));
+		if (!array[i])
+		{
+			free_arr(array, i);
+			return (0);
+		}
+		*(array[i]) = temp->n;
 		temp = temp->next;
 		i++;
 	}
-	i -= 1;
-	k = i;
-	for (j = 0; j <= k / 2 && i >= 0; j++)
+	array[i] = NULL;
+	k = i, i -= 1;
+	for (j = 0; j <= k / 2 && i >= 0; j++, i--)
 	{
-		if (array[i] != array[j])
+		if (*(array[j]) != *(array[i]))
+		{
+			free_arr(array, k);
 			return (0);
-		i -= 1;
+		}
 	}
+	free_arr(array, k);
 	return (1);
 }
+
+
+/**
+  * free_arr - free an array
+  * @array: array
+  * @k: index
+  */
+
+void free_arr(int **array, int k)
+{
+	int j = 0;
+
+	for (j = 0; j < k; j++)
+		free(array[j]);
+	free(array);
+}
+
