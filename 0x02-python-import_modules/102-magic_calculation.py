5651816d7c369d69b612638cf7623d22ab9252fe
patrice02 <patricedev02@gmail.com>
2023-06-14 20:50:32 +0000

    update task

--- 0x02-python-import_modules/102-magic_calculation.py
+++ 0x02-python-import_modules/102-magic_calculation.py
@@ -2,11 +2,12 @@
 
 def magic_calculation(a, b):
     """Function match a bytecode version"""
-    from calculator_1 import add, sub
+    from magic_calculation_102 import add, sub
+
     if a < b:
         c = add(a, b)
         for i in range(4, 6):
             c = add(c, i)
         return c
     else:
-        return (sub(a, b))
+        return sub(a, b)
