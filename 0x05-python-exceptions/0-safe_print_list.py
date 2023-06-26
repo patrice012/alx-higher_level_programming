c47c4417f347e31f37d00649a378911e272dd49b
patrice02 <patricedev02@gmail.com>
2023-06-26 06:48:37 +0000

    add task 0

---
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+
+def safe_print_list(my_list=[], x=0):
+    if isinstance(my_list, list):
+        count = 0
+        for i in range(0, x):
+            try:
+                value = my_list[i]
+            except IndexError:
+                break
+            else:
+                print(value, end="")
+                count += 1
+        print()
+    return count
