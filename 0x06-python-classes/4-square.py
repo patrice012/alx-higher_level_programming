bb3788639010f14fd917716f0859aaf22d5921bb
patrice02 <patricedev02@gmail.com>
2023-06-27 18:03:09 +0000

    add task 4

--- 0x06-python-classes/4-square.py
+++ 0x06-python-classes/4-square.py
@@ -24,6 +24,32 @@ class Square:
 
         self.__size = size
 
+    @property
+    def size(self):
+        """Help to get private attribute value
+
+        Return:
+            size: the size of private attribute
+        """
+        return self.__size
+
+    @size.setter
+    def size(self, size):
+        """ Help to set private attribute value
+
+        Args:
+            size(int): new value of hidden attribute
+
+        Raises:
+            TypeError: When `size` is not Integer
+            ValueError: When `size` is less than 0
+        """
+        if type(size) is not int:
+            raise TypeError("size must be an integer")
+        if size < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = size
+
     def area(self):
         """ Calculate the area ot the given square
 
