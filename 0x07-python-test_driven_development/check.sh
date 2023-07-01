837545fa07d5fb8ef8dae6553a7f56874742c796
patrice02 <patricedev02@gmail.com>
2023-07-01 14:32:16 +0000

    add bash script to automate testing process

--- 0x07-python-test_driven_development/check.sh
+++ 0x07-python-test_driven_development/check.sh
@@ -1,6 +1,14 @@
 #!/bin/bash
 
-
+echo "============  start task 0 ===================="
+echo "Testint started..."
+echo ""
 python3 -m doctest -v ./tests/0-add_integer.txt | tail -2
+echo "Testing ended..."
+echo ""
+echo "Module docstings count:"
 python3 -c 'print(__import__("0-add_integer").__doc__)' | wc -l
+echo "add_integer docstings count:"
 python3 -c 'print(__import__("0-add_integer").add_integer.__doc__)' | wc -l
+echo "================================================"
+
