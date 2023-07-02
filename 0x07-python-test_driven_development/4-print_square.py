bcee5dfad08abf50b492853962dbe30b1eb88e5f
patrice02 <patricedev02@gmail.com>
2023-07-02 14:48:10 +0000

    add task 3

---
+++ 0x07-python-test_driven_development/4-print_square.py
@@ -0,0 +1,30 @@
+"""
+Module contains one function: print_square
+------
+Prototype: def print_square(size)
+---------
+Usage: print_square(integer_number)
+-----
+"""
+
+
+def print_square(size):
+    """
+    Function used to print square of the given number
+
+    Args:
+        size(int):  is the size length of the square
+    Raises:
+        TypeError: When size is not integer
+                or size is float and less than 0
+        ValueError: When size is less than 0
+    """
+    if (size and not (type(size) in (int, float))
+            or (isinstance(size, float) and size < 0)):
+        raise TypeError("size must be an integer")
+    if size < 0:
+        raise ValueError("size must be >= 0")
+    for i in range(0, int(size)):
+        for j in range(0, int(size)):
+            print("#", end="")
+        print()
