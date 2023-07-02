278334cef146e40d340bcd5ab75d40bdd477c2d3
patrice02 <patricedev02@gmail.com>
2023-07-02 14:06:07 +0000

    add task 2

--- 0x07-python-test_driven_development/check.sh
+++ 0x07-python-test_driven_development/check.sh
@@ -24,3 +24,14 @@ echo "function docstings count:"
 python3 -c 'print(__import__("2-matrix_divided").matrix_divided.__doc__)' | wc -l
 echo "================================================"
 
+echo "============  start task 2 ===================="
+echo "Testint started..."
+echo ""
+python3 -m doctest -v ./tests/3-say_my_name.txt | tail -2
+echo "Testing ended..."
+echo ""
+echo "Module docstings count:"
+python3 -c 'print(__import__("3-say_my_name").__doc__)' | wc -l
+echo "function docstings count:"
+python3 -c 'print(__import__("3-say_my_name").say_my_name.__doc__)' | wc -l
+echo "================================================"
