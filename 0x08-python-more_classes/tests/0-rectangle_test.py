060d553f0c3ce95ae2a644d4266232ed7aa594fb
patrice02 <patricedev02@gmail.com>
2023-07-03 15:51:55 +0000

    test doc for module 0-rectangle

--- 0x08-python-more_classes/tests/0-rectangle_test.py
+++ 0x08-python-more_classes/tests/0-rectangle_test.py
@@ -3,18 +3,23 @@
 Unittest for Rectangle class define in 0-rectangle module
 """
 import unittest
-Rectangle = __import__('0-rectangle').Rectangle
+file = __import__('0-rectangle')
 
 class TestEmptyClass(unittest.TestCase):
     """Test case for empty class"""
 
     def setUp(self):
         """Create variable for all tests"""
-        self.rect = Rectangle()
+        self.rect = file.Rectangle()
 
     def test_instance_class(self):
         """Verify the type of class instance"""
-        self.assertTrue(self.rect.__class__ == Rectangle)
+        self.assertTrue(self.rect.__class__ == file.Rectangle)
+
+    def test_docstrings(self):
+        self.assertTrue(len(self.rect.__doc__) > 0)
+        self.assertTrue(len(file.__doc__) > 0)
+
 
 if __name__ == "__main__":
     unittest.main()
