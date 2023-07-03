4f53ff3d0ac8ceb8cf91344e216e1a063416304c
patrice02 <patricedev02@gmail.com>
2023-07-03 18:04:44 +0000

    add task 2

--- 0x08-python-more_classes/2-rectangle.py
+++ 0x08-python-more_classes/2-rectangle.py
@@ -39,7 +39,7 @@ class Rectangle:
             TypeError: if value is not an integer
             ValueError: if value is less than 0
         """
-        if not isinstance(value, int) or not value:
+        if not isinstance(value, int):
             raise TypeError("width must be an integer")
         if value < 0:
             raise ValueError("width must be >= 0")
@@ -60,8 +60,20 @@ class Rectangle:
             TypeError: if value is not an integer
             ValueError: if value is less than 0
         """
-        if not isinstance(value, int) or not value:
+        if not isinstance(value, int):
             raise TypeError("height must be an integer")
         if value < 0:
             raise ValueError("height must be >= 0")
         self.__height = value
+
+    def area(self):
+        """
+        Computes the area of the rectangle
+        """
+        return (self.__width * self.__height)
+
+    def perimeter(self):
+        """
+        Computes the perimeter of the rectangle
+        """
+        return (self.__width + self.__height) * 2
--- 0x08-python-more_classes/tests/2-rectangle_test.py
+++ 0x08-python-more_classes/tests/2-rectangle_test.py
@@ -1,15 +1,17 @@
 #!/usr/bin/python3
 """
-Unittest for Rectangle class define in 0-rectangle module
+Unittest for Rectangle class define in 2-rectangle module
 """
 import unittest
-file = __import__('1-rectangle')
+file = __import__('2-rectangle')
 
 class TestEmptyClass(unittest.TestCase):
     """Test case for empty class"""
 
     def setUp(self):
         self.rect = file.Rectangle()
+        self.rect.height = 6
+        self.rect.width = 4
 
     def test_instance_class(self):
         """Verify the type of class instance"""
@@ -27,10 +29,8 @@ class TestEmptyClass(unittest.TestCase):
             self.assertEqual(self.rect.__height, "__height")
 
     def test_get_and_set_value(self):
-        self.rect.width = 5
-        self.rect.height = 5
-        self.assertEqual(self.rect.height, 5)
-        self.assertEqual(self.rect.width, 5)
+        self.assertEqual(self.rect.height, 6)
+        self.assertEqual(self.rect.width, 4)
 
     def test_invalid_width(self):
         """Test case for invalid width input"""
@@ -51,6 +51,18 @@ class TestEmptyClass(unittest.TestCase):
         """Test case for negative height input"""
         with self.assertRaises(ValueError):
             self.rect.height = -8
+
+    def test_is_public_instance_area_method(self):
+        self.assertEqual(self.rect.area.__name__, "area")
+
+    def test_is_public_instance_perimeter(self):
+        self.assertEqual(self.rect.perimeter.__name__, "perimeter")
+
+    def test_area_method(self):
+        self.assertEqual(self.rect.area(), 24)
+
+    def test_perimeter_instance_method(self):
+        self.assertEqual(self.rect.perimeter(), 20)
 
 
 
