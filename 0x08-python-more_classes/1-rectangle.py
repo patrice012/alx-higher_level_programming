78bbdbe3757ed7d0095cd30b288d10f8b1366088
patrice02 <patricedev02@gmail.com>
2023-07-03 16:59:15 +0000

    add test for task 1

---
+++ 0x08-python-more_classes/1-rectangle.py
@@ -0,0 +1,61 @@
+#!/usr/bin/python3
+"""
+Module contains an empty class Rectangle
+"""
+
+
+class Rectangle:
+    """
+    Empty class
+    """
+    def __init__(self, width=0, height=0):
+        """Create new class instance with default attributes
+
+        Args:
+            width(int): the Width of the rectangle instance
+            height(int): the height of the rectangle instance
+        """
+        self.__width = width
+        self.__height = height
+
+    @property
+    def width(self):
+        """Getter  to retrieve the object width"""
+        return self.__width
+
+    @width.setter
+    def width(self, value):
+        """Set new object width value
+
+        Args:
+            value(int): the new width value
+        Raises:
+            TypeError: if value is not an integer
+            ValueError: if value is less than 0
+        """
+        if not isinstance(value, int) or not value:
+            raise TypeError("width must be an integer")
+        if value < 0:
+            raise ValueError("width must be >= 0")
+        self.__width = value
+
+    @property
+    def height(self):
+        """Getter  to retrieve the object height"""
+        return self.__height
+
+    @height.setter
+    def height(self, value):
+        """Set new object height value
+
+        Args:
+            value(int): the new height value
+        Raises:
+            TypeError: if value is not an integer
+            ValueError: if value is less than 0
+        """
+        if not isinstance(value, int) or not value:
+            raise TypeError("height must be an integer")
+        if value < 0:
+            raise ValueError("height must be >= 0")
+        self.__height = value
--- 0x08-python-more_classes/check.sh
+++ 0x08-python-more_classes/check.sh
@@ -1,3 +1,5 @@
 #!/bin/bash
 echo "Test task 0"
 python3 -m unittest tests.0-rectangle_test
+echo "Test task 1"
+python3 -m unittest tests.1-rectangle_test
