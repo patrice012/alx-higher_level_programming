c55967eee0df577d532e7e6d9769006435ea2b05
patrice02 <patricedev02@gmail.com>
2023-07-03 18:03:59 +0000

    update task 1

--- 0x08-python-more_classes/1-rectangle.py
+++ 0x08-python-more_classes/1-rectangle.py
@@ -39,7 +39,7 @@ class Rectangle:
             TypeError: if value is not an integer
             ValueError: if value is less than 0
         """
-        if not isinstance(value, int) or not value:
+        if not isinstance(value, int):
             raise TypeError("width must be an integer")
         if value < 0:
             raise ValueError("width must be >= 0")
@@ -60,7 +60,7 @@ class Rectangle:
             TypeError: if value is not an integer
             ValueError: if value is less than 0
         """
-        if not isinstance(value, int) or not value:
+        if not isinstance(value, int):
             raise TypeError("height must be an integer")
         if value < 0:
             raise ValueError("height must be >= 0")
