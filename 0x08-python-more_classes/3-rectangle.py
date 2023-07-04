bf3098de34e13e925c9c54fc33d876b24068ee6e
patrice02 <patricedev02@gmail.com>
2023-07-04 20:31:59 +0000

    update __init__ method to check more conditions

--- 0x08-python-more_classes/3-rectangle.py
+++ 0x08-python-more_classes/3-rectangle.py
@@ -21,6 +21,15 @@ class Rectangle:
             width(int): the Width of the rectangle instance
             height(int): the height of the rectangle instance
         """
+        if not isinstance(width, int):
+            raise TypeError("width must be an integer")
+        if not isinstance(height, int):
+            raise TypeError("height must be an integer")
+        if width < 0:
+            raise ValueError("width must be >= 0")
+        if height < 0:
+            raise ValueError("height must be >= 0")
+
         self.__width = width
         self.__height = height
 
