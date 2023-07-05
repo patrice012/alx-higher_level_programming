355e2aa226d20dddf61dbc84ed4a8547573e5d31
patrice02 <patricedev02@gmail.com>
2023-07-05 10:59:57 +0000

    fix error in task 3

--- 0x07-python-test_driven_development/3-say_my_name.py
+++ 0x07-python-test_driven_development/3-say_my_name.py
@@ -40,7 +40,4 @@ def say_my_name(first_name, last_name=""):
         pass
     else:
         raise TypeError("last_name must be a string")
-    if last_name:
-        print("My name is {} {}".format(first_name, last_name))
-    else:
-        print("My name is {}".format(first_name))
+    print("My name is {} {}".format(first_name, last_name))
