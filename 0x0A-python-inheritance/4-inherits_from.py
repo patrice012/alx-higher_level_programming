fdc5e37bf73cbfbcba8a1fe21a59bcca2b11c6c5
patrice02 <patricedev02@gmail.com>
2023-07-10 18:59:12 +0000

    add task 4

---
+++ 0x0A-python-inheritance/4-inherits_from.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+"""
+Module contains Function inherits_from
+
+Prototype: def inherits_from(obj, a_class)
+"""
+
+
+def inherits_from(obj, a_class):
+    """
+    Check if the object is an instance of a class
+    that inherited (directly or indirectly) from
+    the specified class; otherwise False.
+    """
+    return issubclass(type(obj), a_class) and type(obj) != a_class
