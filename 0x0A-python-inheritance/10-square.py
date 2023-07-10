ebe720bc4b490a0ccea4169f396e8e9b50c40342
patrice02 <patricedev02@gmail.com>
2023-07-10 22:20:36 +0000

    add tasks 10 and 11

---
+++ 0x0A-python-inheritance/10-square.py
@@ -0,0 +1,30 @@
+#!/usr/bin/python3
+"""
+Module contains Square Class
+"""
+
+Rectangle = __import__('9-rectangle').Rectangle
+
+
+class Square(Rectangle):
+    """
+    A Square class
+    """
+    def __init__(self, size):
+        """
+        args:
+            size (int): size of square
+        """
+        self.integer_validator("size", size)
+        self.__size = size
+
+    def area(self):
+        """
+        Implementation of area method
+        """
+        return self.__size * self.__size
+
+    def __str__(self):
+        """Custom function"""
+        name = self.__class__.__name__
+        return "[{}] {}/{}".format("Rectangle", self.__size, self.__size)
