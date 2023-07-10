41e9f020e7fed55ee02e4f9fd5c75d0ca4cc1f4b
patrice02 <patricedev02@gmail.com>
2023-07-10 20:57:11 +0000

    add task 6

---
+++ 0x0A-python-inheritance/6-base_geometry.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+"""
+Module contains empty class BaseGeometry
+"""
+
+
+class BaseGeometry:
+    """
+    An empty class BaseGeometry
+    """
+    def area(self):
+        """
+        Empty area method
+
+        Raises:
+            Exception
+        """
+        raise Exception('area() is not implemented')
