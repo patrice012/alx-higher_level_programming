5dfff4c5e5b0b2ceef543d310cd501a040183834
patrice02 <patricedev02@gmail.com>
2023-09-15 20:15:33 +0000

    update docstring

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -2,12 +2,22 @@
 
 """
 Module contains script that lists all states from the database hbtn_0e_0_usa
+1-Create database if not exist
+2-populated db
+3-run script cat 0-select_states.sql | mysql -uroot -p
 """
 
 
 def connect_db(host, port, user, passwd, db, **kwargs):
     """
     Connect MySQLdb to a Database
+
+    Args:
+        host(str or int): Database host to use
+        post(int): DB port
+        user(str): DB user to use
+        passwd(str): DB user's password
+        db(str): database name to connect to
 
     Return:
         cur(cursor): cursor object
@@ -21,7 +31,7 @@ def connect_db(host, port, user, passwd, db, **kwargs):
 
 def print_data(datas):
     """
-    Helper function use to print data
+    Helper function used to print data
     """
     for data in datas:
         print(data)
@@ -42,7 +52,7 @@ def get_all_data(av):
     cur = connect_db(host, port, user=username, passwd=password, db=db_name)
 
     # execute raw sql
-    cur.execute("SELECT * FROM states ORDER BY id")
+    cur.execute("SELECT * FROM states ORDER BY id ASC")
     states = cur.fetchall()
     return states
 
