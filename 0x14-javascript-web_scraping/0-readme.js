98428901835ddd182254a6803d30012b052a1db1
patrice02 <patricedev02@gmail.com>
2023-10-24 17:16:15 +0000

    add task 0

---
+++ 0x14-javascript-web_scraping/0-readme.js
@@ -0,0 +1,11 @@
+#!/usr/bin/node
+const fs = require('fs');
+const file = process.argv[2];
+
+fs.readFile(file, 'utf-8', (error, data) => {
+  if (error) {
+    console.log(error);
+    return;
+  }
+  console.log(data);
+});
